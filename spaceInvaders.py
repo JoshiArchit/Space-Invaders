0cfa2c53b40a14a381c6c16c9c3fa96a1de2a48e
architJoshi2904 <aj6082@rit.edu>
2024-06-02 12:44:49 -0400

    multiple enemy generation and movement adjustments

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -27,34 +27,70 @@ class Game:
         self.clock = pygame.time.Clock()
 
         # Enemy rectangle (top-left = 20, bottom-left=50, width & height = 20)
-        self.enemy_pos = (20, 50)
-        self.enemy = pygame.Rect(20, 50, 20, 20)
-        self.enemy_speed = 10  # Will move continuously
+        # self.enemy = pygame.Rect(10, 50, 20, 20)
+        self.enemies = self.render_enemy()
+        self.enemy_speed = 2  # Will move continuously
         # Todo: Reduce frames as difficulty increases
         self.enemy_update_delay = 25  # Number of frames between each movement
         self.enemy_update_counter = 0
 
         # Player rectangle (top-left = 150, bottom-left = 200, width & height = 20)
-        self.player_pos = (150, 200)
-        self.player = pygame.Rect(150, 200, 20, 20)
+        self.player = pygame.Rect(150, 200, 10, 10)
         self.player_speed = 5
+
+    def render_enemy(self):
+        """
+        Helper to create a list of enemy objects (Rectangles) on screen.
+        The list will have gaps between each enemy.
+        If collision is detected, remove the enemy from the list.
+        :return:
+        """
+        enemies = []
+        enemy = pygame.Rect(15, 50, 10, 10)
+        enemies.append(enemy)
+        next_enemy_x = enemy.right + 10
+        while next_enemy_x < SURFACE_WIDTH:
+            enemy = pygame.Rect(next_enemy_x, 50, 10, 10)
+            # Return if last enemy goes out of bounds
+            if enemy.right >= SURFACE_WIDTH:
+                return enemies
+            enemies.append(enemy)
+            next_enemy_x = enemy.right + 10
+        return enemies
+
+    # def movement(self):
+    #     """
+    #     Helper function for jagged enemy movement.
+    #
+    #     :return: None
+    #     """
+    #     self.enemy_update_counter += 1
+    #     if self.enemy_update_counter >= self.enemy_update_delay:
+    #         self.enemy_update_counter = 0
+    #         self.enemy.x += self.enemy_speed
+    #         if self.enemy.left < 0:
+    #             self.enemy.x = 0
+    #             self.enemy_speed = abs(self.enemy_speed)
+    #         elif self.enemy.right >= SURFACE_WIDTH:
+    #             self.enemy.right = SURFACE_WIDTH
+    #             self.enemy_speed = -abs(self.enemy_speed)
 
     def movement(self):
         """
         Helper function for jagged enemy movement.
-
-        :return:
+        :return: None
         """
         self.enemy_update_counter += 1
         if self.enemy_update_counter >= self.enemy_update_delay:
             self.enemy_update_counter = 0
-            self.enemy.x += self.enemy_speed
-            if self.enemy.left < 0:
-                self.enemy.x = 0
-                self.enemy_speed = abs(self.enemy_speed)
-            elif self.enemy.right >= SURFACE_WIDTH:
-                self.enemy.right = SURFACE_WIDTH
-                self.enemy_speed = -abs(self.enemy_speed)
+            for enemy in self.enemies:
+                enemy.x += self.enemy_speed
+                if enemy.left < 0:
+                    enemy.x = 0
+                    self.enemy_speed = abs(self.enemy_speed)
+                elif enemy.right >= SURFACE_WIDTH:
+                    enemy.right = SURFACE_WIDTH
+                    self.enemy_speed = -abs(self.enemy_speed)
 
     def run(self):
         # game loop to display window
@@ -71,9 +107,10 @@ class Game:
             self.display.fill((0, 0, 0))
             # Display player and enemy on display
             pygame.draw.rect(self.display, (255, 255, 255), self.player)
-            pygame.draw.rect(self.display, (255, 0, 0), self.enemy)
             # Todo: Needs to have a separate render enemy function that renders list of enemies
-            # Update enemy movement
+            for enemy in self.enemies:
+                pygame.draw.rect(self.display, (255, 0, 0), enemy)
+            # Update every enemy's position
             self.movement()
 
             # Blit display to screen
