1a30d2593a2f6a1a38ab0d92a69a62cda5a8e305
architJoshi2904 <aj6082@rit.edu>
2024-06-02 17:52:40 -0400

    Code refactoring to accomodate Entity class and object references

--- updates.py
+++ updates.py
@@ -14,47 +14,14 @@ SURFACE_WIDTH = 320
 SURFACE_HEIGHT = 240
 
 
-# # Original movement with dictionary
-# def movement(game):
-#     """
-#     Helper function for jagged enemy movement.
-#     :return: None
-#     """
-#     game.enemy_update_counter += 1
-#     if game.enemy_update_counter >= game.enemy_update_delay:
-#         game.enemy_update_counter = 0
-#
-#         # Check the leftmost and rightmost enemies
-#         leftmost_enemy = game.enemies[min(game.enemies.keys())][0]
-#         rightmost_enemy = game.enemies[max(game.enemies.keys())][0]
-#
-#         # Doesn't matter if they are alive or dead. They will move together. Alive or dead is
-#         # handled in collision
-#         for enemy, info in game.enemies.items():
-#             enemy_rect = info[0]  # Rect object is the first element in the tuple
-#             enemy_rect.x += game.enemy_speed
-#
-#         # Check if the leftmost enemy has hit the left wall
-#         if leftmost_enemy.left <= 0:
-#             game.enemy_speed = abs(game.enemy_speed)
-#             for enemy, info in game.enemies.items():
-#                 enemy_rect = info[0]
-#                 enemy_rect.y += 10
-#         # Check if the rightmost enemy has hit the right wall
-#         elif rightmost_enemy.right >= SURFACE_WIDTH:
-#             game.enemy_speed = -abs(game.enemy_speed)
-#             for enemy, info in game.enemies.items():
-#                 enemy_rect = info[0]
-#                 enemy_rect.y += 10
-
-def movement(self):
-    self.enemy_update_counter += 1
-    if self.enemy_update_counter >= self.enemy_update_delay:
-        self.enemy_update_counter = 0
+def movement(enemy_obj):
+    enemy_obj.enemy_update_counter += 1
+    if enemy_obj.enemy_update_counter >= enemy_obj.enemy_update_delay:
+        enemy_obj.enemy_update_counter = 0
 
         # Check the leftmost and rightmost enemies
-        leftmost_enemy = self.enemies[0][0]
-        rightmost_enemy = self.enemies[len(self.enemies) - 1][0]
+        leftmost_enemy = enemy_obj.enemies[0][0]
+        rightmost_enemy = enemy_obj.enemies[len(enemy_obj.enemies) - 1][0]
 
         # Use left_most and right_most to calculate bounds for the row
         left_most = leftmost_enemy.left
@@ -62,36 +29,36 @@ def movement(self):
 
         # Check if the leftmost enemy has hit the left wall
         if left_most <= 0:
-            self.enemy_speed = abs(self.enemy_speed)
-            for enemy_rect in self.enemies:
+            enemy_obj.enemy_speed = abs(enemy_obj.enemy_speed)
+            for enemy_rect in enemy_obj.enemies:
                 enemy_rect[0].y += 10
         # Check if the rightmost enemy has hit the right wall
         elif right_most >= SURFACE_WIDTH:
-            self.enemy_speed = -abs(self.enemy_speed)
-            for enemy_rect in self.enemies:
+            enemy_obj.enemy_speed = -abs(enemy_obj.enemy_speed)
+            for enemy_rect in enemy_obj.enemies:
                 enemy_rect[0].y += 10
 
         # Move all enemies
-        for enemy in self.enemies:
-            enemy[0].x += self.enemy_speed
+        for enemy in enemy_obj.enemies:
+            enemy[0].x += enemy_obj.enemy_speed
 
 
-def fire_bullet(game):
+def fire_bullet(bullet_obj, player_obj):
     """
     Helper function to fire a bullet from the player object.
     :return: None
     """
-    if game.bullet_fired:
+    if bullet_obj.bullet_fired:
         # Move bullet up (subtract speed)
-        game.bullet.y -= game.bullet_speed
+        bullet_obj.bullet.y -= bullet_obj.bullet_speed
         # Out of bounds condition
-        if game.bullet.top <= 0:
-            game.bullet_fired = False
+        if bullet_obj.bullet.top <= 0:
+            bullet_obj.bullet_fired = False
             # Reset y to player center
-            game.bullet.y = game.player.centery
+            bullet_obj.bullet.y = player_obj.player.centery
 
 
-def collision(game):
+def collision(bullet_obj, enemy_obj, player_obj):
     """
     Helper function to detect collision between bullet and enemies. Successful collision
     removes enemy from list.
@@ -107,23 +74,15 @@ def collision(game):
     #             game.bullet.y = game.player.centery
     #             game.enemies[enemy] = (rect_enemy, False)
 
-    if game.bullet_fired:
-        for enemy in game.enemies:
+    if bullet_obj.bullet_fired:
+        for enemy in enemy_obj.enemies:
             rect_enemy = enemy[0]
-            if rect_enemy.colliderect(game.bullet):
-                game.bullet_fired = False
-                game.bullet.y = game.player.centery
-                game.enemies.remove(enemy)
-                game.score += 1
-
-
-def scorebar(game):
-    # Display score and lives
-    font = pygame.font.Font(None, 36)
-    score_text = font.render(f"Score: {game.score}", True, (255, 255, 255))
-    lives_text = font.render(f"Lives: {game.lives}", True, (255, 255, 255))
-    game.display.blit(score_text, (10, 10))
-    game.display.blit(lives_text, (10, 40))
+            if rect_enemy.colliderect(bullet_obj.bullet):
+                bullet_obj.bullet_fired = False
+                bullet_obj.bullet.y = player_obj.player.centery
+                enemy_obj.enemies.remove(enemy)
+                return 1
+    return 0
 
 
 def render_enemy():
@@ -133,19 +92,6 @@ def render_enemy():
     If collision is detected, remove the enemy from the list.
     :return:
     """
-    # # ORIGINAL enemies dictionary to store enemy objects and alive status
-    # enemies = list()
-    # enemy_y = 50
-    # enemy_x = 15
-    # counter = 0
-    # while True:
-    #     enemies[counter] = (pygame.Rect(enemy_x, enemy_y, 10, 10), True)
-    #     enemy_x += enemies[counter][0].width + 10
-    #     if enemy_x >= SURFACE_WIDTH or enemies[counter][0].right > SURFACE_WIDTH:
-    #         enemies.pop(counter)
-    #         return enemies
-    #     counter += 1
-
     # List of enemies
     enemies = list()
     enemy_y = 50
@@ -160,18 +106,19 @@ def render_enemy():
         counter += 1
 
 
-def player_movement(game):
+def player_movement(player_obj, bullet_obj):
     """
     Helper function to move the player object.
     :return: None
     """
     # Move player
-    if game.move_left and game.player.left > 0:
-        game.player.x -= game.player_speed
-    if game.move_right and game.player.right < SURFACE_WIDTH:
-        game.player.x += game.player_speed
+    if player_obj.move_left and player_obj.player.left > 0:
+        player_obj.player.x -= player_obj.player_speed
+    if player_obj.move_right and player_obj.player.right < SURFACE_WIDTH:
+        player_obj.player.x += player_obj.player_speed
 
     # Update bullet position with player's position
-    if not game.bullet_fired:
-        game.bullet.x = game.player.centerx - 2
-        game.bullet.y = game.player.centery
+    if not bullet_obj.bullet_fired:
+        bullet_obj.bullet.x = player_obj.player.centerx - 2
+        bullet_obj.bullet.y = player_obj.player.centery
+
